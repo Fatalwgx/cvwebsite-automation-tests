779bd021a4e75a54b57a826f6d091cc6f6714999
Fatalwgx <nothingfatalleo@gmail.com>
2023-06-04 10:15:30 +0300

    selenoid capabilities fixture autouse set to True

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,7 +37,7 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
